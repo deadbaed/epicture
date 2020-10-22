42c03aa6ac90dc90b5e009656913a3d1488a4717
Pierre <barillec.pierre@gmail.com>
2020-10-22 18:56:10 +0200

    ADD - upload via api imgur

--- app/src/main/java/com/philippeloctaux/epicture/UploadActivity.kt
+++ app/src/main/java/com/philippeloctaux/epicture/UploadActivity.kt
@@ -3,24 +3,41 @@ package com.philippeloctaux.epicture
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.app.Fragment
+import android.content.ClipDescription
+import android.graphics.Bitmap
+import android.util.Base64
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentManager
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.philippeloctaux.epicture.R
+import com.philippeloctaux.epicture.api.Imgur
+import com.philippeloctaux.epicture.api.types.ImageListResponse
+import com.philippeloctaux.epicture.api.types.UploadResponse
+import com.philippeloctaux.epicture.ui.upload.UploadFragment
 import kotlinx.android.synthetic.main.activity_upload.*
 import kotlinx.android.synthetic.main.fragment_upload.*
+import retrofit2.Call
+import retrofit2.Response
+import java.io.ByteArrayOutputStream
+import java.io.File
+import java.util.ArrayList
 
 class UploadActivity : AppCompatActivity() {
+
+    private var my_image: Bitmap? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_upload)
+
         val image = intent.getStringExtra("Image")
         image_view.setImageURI(Uri.parse(image))
         val cancel_button: Button? = findViewById(R.id.button_cancel)
@@ -36,9 +53,41 @@ class UploadActivity : AppCompatActivity() {
             val temp_descr = description?.getText().toString()
             if (temp_title.isEmpty()) {
                 Toast.makeText(this, "Title is required", Toast.LENGTH_SHORT).show();
+            } else {
+                UploadImage(image, temp_title, temp_descr)
             }
-            println("le titre est $temp_title et la description est $temp_descr")
         }
+    }
+
+    private fun UploadImage(image: String, title: String, description: String) {
+        val byteA: ByteArrayOutputStream = ByteArrayOutputStream()
+        image?.compress(Bitmap.CompressFormat.PNG, 100, byteA)
+        val byteArray = byteA.toByteArray()
+        val encoded = Base64.encodeToString(byteArray, Base64.DEFAULT)
+        val client = Imgur.create()
+        val settings = Settings(applicationContext)
+        val apiRequest =
+            client.uploadImage("Bearer " + settings.getValue(settings.accessToken), image, title, description)
+
+        // make request and wait for response
+        apiRequest.enqueue(object : retrofit2.Callback<UploadResponse> {
+
+            // on success
+            override fun onResponse(
+                call: Call<UploadResponse>,
+                response: Response<UploadResponse>
+            ) {
+                // return to home
+                redirectToMainActivity()
+            }
+
+            // on failure
+            override fun onFailure(call: Call<UploadResponse>, t: Throwable) {
+                Toast.makeText(applicationContext, "Failed to upload pictures", Toast.LENGTH_SHORT)
+                    .show()
+            }
+        })
+
     }
 
     private fun redirectToMainActivity() {
--- app/src/main/java/com/philippeloctaux/epicture/api/Imgur.kt
+++ app/src/main/java/com/philippeloctaux/epicture/api/Imgur.kt
@@ -1,11 +1,11 @@
 package com.philippeloctaux.epicture.api
 
 import com.philippeloctaux.epicture.api.types.ImageListResponse
+import com.philippeloctaux.epicture.api.types.UploadResponse
 import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.http.GET
-import retrofit2.http.Header
+import retrofit2.http.*
 
 interface Imgur {
     companion object {
@@ -19,4 +19,13 @@ interface Imgur {
 
     @GET("account/me/images")
     fun getAccountImages(@Header("Authorization") accessToken: String): Call<ImageListResponse>
+
+    @FormUrlEncoded
+    @POST("upload")
+    fun uploadImage(
+        @Header("Authorization") accessToken: String,
+        @Field("image") image: String,
+        @Field("title") title: String,
+        @Field("description") description: String
+    ): Call<UploadResponse>
 }
