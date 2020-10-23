54f4cc525e95d7c168f71e771fbd7df9f28e01ea
Philippe Loctaux <p@philippeloctaux.com>
2020-10-23 14:41:21 +0200

    added toolbar in upload activity, wola pierre tu l'as design ton comme un porc

--- app/src/main/java/com/philippeloctaux/epicture/UploadActivity.kt
+++ app/src/main/java/com/philippeloctaux/epicture/UploadActivity.kt
@@ -3,24 +3,26 @@ package com.philippeloctaux.epicture
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
-import com.philippeloctaux.epicture.R
+import androidx.appcompat.widget.Toolbar
 import kotlinx.android.synthetic.main.activity_upload.*
-import kotlinx.android.synthetic.main.fragment_upload.*
 
 class UploadActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_upload)
+
+        // setup up button to go back to previous activity
+        val toolbar : Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        val actionBar = supportActionBar
+        actionBar?.setDisplayHomeAsUpEnabled(true)
+
         val image = intent.getStringExtra("Image")
         image_view.setImageURI(Uri.parse(image))
         val cancel_button: Button? = findViewById(R.id.button_cancel)
@@ -35,7 +37,7 @@ class UploadActivity : AppCompatActivity() {
             val temp_title = title?.getText().toString()
             val temp_descr = description?.getText().toString()
             if (temp_title.isEmpty()) {
-                Toast.makeText(this, "Title is required", Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, "Title is required", Toast.LENGTH_SHORT).show()
             }
             println("le titre est $temp_title et la description est $temp_descr")
         }
@@ -45,4 +47,14 @@ class UploadActivity : AppCompatActivity() {
         startActivity(Intent(this, MainActivity::class.java))
         finish()
     }
+
+    // up button goes to previous fragment instead to default fragment in parent activity
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        super.onOptionsItemSelected(item)
+        when (item.itemId) {
+            android.R.id.home -> finish()
+        }
+        return true
+    }
+
 }
